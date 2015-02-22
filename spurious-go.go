2f9d91e49a2b45d47c2fc86bf8db1e67535ce2fd
Steven Jack <stevenmajack@gmail.com>
2015-02-22 19:53:20 +0000

    Adds commands

--- spurious-go.go
+++ spurious-go.go
@@ -9,8 +9,32 @@ func main() {
   app := cli.NewApp()
   app.Name = "spurious"
   app.Usage = "Spurious is a toolset allowing development against a subset of AWS resource, locally."
-  app.Action = func(c *cli.Context) {
-    println("boom! I say!")
+
+  app.Commands = []cli.Command{
+    {
+      Name:      "init",
+      Usage:     "Pulls down the images for and creates the containers.",
+      Action: func(c *cli.Context) {
+        println("Intialized containers", c.Args().First())
+      },
+    },
+    {
+      Name:      "start",
+      ShortName: "s",
+      Usage:     "Starts the containers for running the services.",
+      Action: func(c *cli.Context) {
+        println("Starts containers", c.Args().First())
+      },
+    },
+    {
+      Name:      "stop",
+      ShortName: "st",
+      Usage:     "Stops the containers for the spurious services.",
+      Action: func(c *cli.Context) {
+        println("Stops containers", c.Args().First())
+      },
+    },     
   }
+
   app.Run(os.Args)
 }
