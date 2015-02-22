1df6acfdd64fc85971385034e3ecfad1413f6c4d
Steven Jack <stevenmajack@gmail.com>
2015-02-22 19:36:52 +0000

    Lowercase app name

--- spurious-go.go
+++ spurious-go.go
@@ -7,7 +7,10 @@ import (
 
 func main() {
   app := cli.NewApp()
-  app.Name = "Spurious"
+  app.Name = "spurious"
   app.Usage = "Spurious is a toolset allowing development against a subset of AWS resource, locally."
+  app.Action = func(c *cli.Context) {
+    println("boom! I say!")
+  }
   app.Run(os.Args)
 }
